7ecab44ec3bcc0e3a8d9e4672952d4cf91b16972
MetaGauravSaini <gaurav.saini414@gmail.com>
2019-05-17 14:50:42 +0530

    updated onboardinf redirect url and auth success page

--- api/routes/oauth.js
+++ api/routes/oauth.js
@@ -38,7 +38,7 @@ module.exports = (app, controller) => {
                     }
                     auth.identity = identity;
                     controller.trigger('oauth_success', [auth]);
-                    res.redirect(auth.identity.url);
+                    res.redirect('https://slack.com/app_redirect?app=AEXKDD4UC');
                 });
             });
         }
