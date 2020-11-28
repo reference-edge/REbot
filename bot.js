52ed25d1b5a0d7b0fa7bb40ae4c48d3b2122bed0
Rahul Sharma <38658846+RahulSharma92@users.noreply.github.com>
2020-11-28 19:11:31 +0530

    update to granular scopes.

--- bot.js
+++ bot.js
@@ -11,7 +11,15 @@ const { getFilterMiddleware } = require('./listeners/middleware/migration-filter
 let botCfg = {
     clientId: process.env.SLACK_CLIENT_ID,
     clientSecret: process.env.SLACK_CLIENT_SECRET,
-    scopes: ['bot', 'team:read', 'users:read', 'users:read.email', 'channels:write'],
+    //update to granular scopes 
+    scopes: ['channels:join',
+        'channels:manage',
+        'channels:read',
+        'incoming-webhook',
+        'team:read',
+        'users:read',
+        'users:read.email',
+        'users:write', 'team:read', 'users:read', 'users:read.email', 'channels:write'],
     storage: mongoProvider,
     clientSigningSecret: process.env.SLACK_SIGNING_SECRET
 };
