5577c64eb184c0d729c79f6a11e6974f3a8da8d8
Rahul Sharma <38658846+RahulSharma92@users.noreply.github.com>
2020-11-30 17:44:19 +0530

    test slack to sf connection

--- util/refedge.js
+++ util/refedge.js
@@ -5,7 +5,7 @@ const logger = require('../common/logger');
 module.exports = {
     saveTeamId: (conn, teamData) => {
         //////####### only for DEV purpose, do not push and revert back to refedge
-        conn.apex.post('/pordev/rebot', teamData, (err, res) => {
+        conn.apex.post('/PORDEV/rebot', teamData, (err, res) => {
 
             if (err) {
                 logger.log(err);
